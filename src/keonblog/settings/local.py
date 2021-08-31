0ec56e423235612fcee7354b213c1e3398ac2a1c
김건순 <keonsunkim@gimgeonsuns-MacBook-Air.local>
2021-08-31 15:30:18 +0900

    Revert "locals"
    This reverts commit cb6eba162e2ec74674f76b5249876e94ae756cea.

--- src/keonblog/settings/local.py
+++ src/keonblog/settings/local.py
@@ -1,25 +1,8 @@
-# locals.py
 import os
-
-## import dotenv module
-from dotenv import load_dotenv
-
-## import pahtlib
-from pathlib import Path
 
 from django.core.exceptions import ImproperlyConfigured
 
 from .base import *
-
-# load dotenv variables to env
-def get_dotenv():
-  env_dir = Path(__file__).resolve().parents[4]
-  env_path = Path(env_dir)/'.env'
-
-  if env_path.exists():
-    load_dotenv(dotenv_path=env_path)
-
-
 
 def get_env_variable(var_name):
     """Get environment variable from os"""
@@ -29,22 +12,19 @@ def get_env_variable(var_name):
         error_msg = f'Set the {var_name} environment variable'
         raise ImproperlyConfigured(error_msg)
 
-def get_json_secret(var_name, secret_file_name):
+def get_json_secret(setting, secret_file_name):
     """ Get environment variable from json file"""
     with open(secret_file_name) as f:
         secrets = json.loads(f.read())
 
         try:
-            return secrets[var_name]
+            return secrets[settings]
 
         except KeyError:
             error_msg = f'Set the {setting} environment variable'
             raise ImproperlyConfigured(error_msg)
 
-
-get_dotenv()
-
-SECRET_KEY = get_env_variable('SECRET_KEY')
+SECRET_KEY = get_env_variable('DJANGO_SECRET_KEY')
 # Debugging Settings only allow True in developing versions
 DEBUG = True
 
@@ -60,9 +40,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 DATABASES={
    'default':{
       'ENGINE':'django.db.backends.postgresql_psycopg2',
-      'NAME': get_env_variable('DB_NAME'),
-      'USER': get_env_variable('USER'),
-      'PASSWORD': get_env_variable('PASSWORD'),
+      'NAME':'postgres',
+      'USER':'postgres',
+      'PASSWORD': get_env_variable('KEONBLOGLOCALPG_PW'),
       'HOST':'localhost',
       'PORT':'5432',
    }
--- src/keonblog/settings/test.py
+++ src/keonblog/settings/test.py
@@ -1,25 +1,12 @@
-# locals.py
 import os
-
-## import dotenv module
-from dotenv import load_dotenv
-
-## import pahtlib
-from pathlib import Path
-
 from django.core.exceptions import ImproperlyConfigured
 
-from .base import *
+BASE_DIR = os.path.dirname(os.path.dirname(
+                os.path.dirname(os.path.abspath(__file__))))
 
-# load dotenv variables to env
-def get_dotenv():
-  env_dir = Path(__file__).resolve().parents[4]
-  env_path = Path(env_dir)/'.env'
-
-  if env_path.exists():
-    load_dotenv(dotenv_path=env_path)
-
-
+def root(*dirs):
+    base_dir = os.path.join(os.path.dirname(__file__), '..', '..')
+    return os.path.abspath(os.path.join(base_dir, *dirs))
 
 def get_env_variable(var_name):
     """Get environment variable from os"""
@@ -29,41 +16,7 @@ def get_env_variable(var_name):
         error_msg = f'Set the {var_name} environment variable'
         raise ImproperlyConfigured(error_msg)
 
-def get_json_secret(var_name, secret_file_name):
-    """ Get environment variable from json file"""
-    with open(secret_file_name) as f:
-        secrets = json.loads(f.read())
 
-        try:
-            return secrets[var_name]
+SECRET_KEY = get_env_variable('DJANGO_SECRET_KEY')
 
-        except KeyError:
-            error_msg = f'Set the {setting} environment variable'
-            raise ImproperlyConfigured(error_msg)
-
-
-get_dotenv()
-
-SECRET_KEY = get_env_variable('SECRET_KEY')
-# Debugging Settings only allow True in developing versions
-DEBUG = True
-
-# Following os path can be changed later benchmarking twoscoops of django pg98
-# In production, we could use different sources to feed cdns, nginx, ...
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES={
-   'default':{
-      'ENGINE':'django.db.backends.postgresql_psycopg2',
-      'NAME': get_env_variable('DB_NAME'),
-      'USER': get_env_variable('USER'),
-      'PASSWORD': get_env_variable('PASSWORD'),
-      'HOST':'localhost',
-      'PORT':'5432',
-   }
-}
+print(BASE_DIR)
